a598296b755b3fbc1497efb43fc913558edc3f0d
TYNITE <bautistamonachesi@gmail.com>
2025-04-16 14:35:13 -0300

    Actualizacion de TYNITE 1.2 Ver 2

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 
+// Crear la aplicación Express
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Middleware para servir archivos estáticos desde la carpeta "public"
+// Servir archivos estáticos desde la carpeta "public"
 app.use(express.static(__dirname + "/public"));
 
-// Ruta predeterminada para servir "index.html" cuando accedas a "/"
+// Ruta principal para servir "index.html"
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
@@ -18,7 +19,12 @@ app.get("/", (req, res) => {
 io.on("connection", (socket) => {
   console.log(`Usuario conectado: ${socket.id}`);
 
-  // Escuchar mensajes del cliente
+  // Recibir notificación de conexión de un usuario
+  socket.on("user connected", (data) => {
+    console.log(`Usuario conectado: ${data.username} con color ${data.color}`);
+  });
+
+  // Escuchar mensajes de texto enviados por los clientes
   socket.on("chat message", (data) => {
     if (data && data.username && data.message) {
       console.log(`Mensaje recibido de ${data.username}: ${data.message}`);
@@ -29,7 +35,32 @@ io.on("connection", (socket) => {
     }
   });
 
-  // Evento cuando un usuario se desconecta
+  // Escuchar archivos (imágenes/videos) enviados por los clientes
+  socket.on("file upload", (data) => {
+    if (data && data.username && data.file && data.fileType) {
+      console.log(`Archivo recibido de ${data.username}: ${data.fileType}`);
+      // Reenviar el archivo a todos los clientes conectados
+      io.emit("file upload", data);
+    } else {
+      console.log("Archivo inválido recibido:", data);
+    }
+  });
+
+  // Escuchar cambio de nombre
+  socket.on("name change", (data) => {
+    console.log(`El usuario ${data.previousName} cambió su nombre a ${data.newName}`);
+    // Reenviar el evento a todos los clientes conectados
+    io.emit("name change", data);
+  });
+
+  // Escuchar cambio de color
+  socket.on("color change", (data) => {
+    console.log(`El usuario ${data.username} cambió su color a ${data.color}`);
+    // Reenviar el evento a todos los clientes conectados
+    io.emit("color change", data);
+  });
+
+  // Evento cuando un cliente se desconecta
   socket.on("disconnect", () => {
     console.log(`Usuario desconectado: ${socket.id}`);
   });
